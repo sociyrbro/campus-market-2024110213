0c5c1acfc20e43f87ccc1882b3b8ec9e2f09a46a
sociyrbro <1565321242@qq.com>
2026-06-28 09:51:39 +0800

    看板页设计 显示简单统计信息

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
 
 <template>
   <nav>
-    <router-link to="/home">首页</router-link>
-    <router-link to="/list">列表</router-link>
-    <router-link to="/publish">发布</router-link>
-    <router-link to="/message">消息</router-link>
+    <router-link to="/home">首页 </router-link>
+    <router-link to="/list">列表 </router-link>
+    <router-link to="/publish">发布 </router-link>
+    <router-link to="/message">消息 </router-link>
     <router-link to="/profile">我的</router-link>
   </nav>
 
