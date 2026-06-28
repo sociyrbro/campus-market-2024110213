f0ce17b20f17b707ff88f97ba646de7b93634342
sociyrbro <1565321242@qq.com>
2026-06-28 15:28:31 +0800

    配置路由文件并确认再main.ts中挂载路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+
+import HomeView from '../views/HomeView.vue'
+import TradeView from '../views/TradeView.vue'
+import LostFoundView from '../views/LostFoundView.vue'
+import GroupBuyView from '../views/GroupBuyView.vue'
+import ErrandView from '../views/ErrandView.vue'
+import PublishView from '../views/PublishView.vue'
+import MessageView from '../views/MessageView.vue'
+import UserCenterView from '../views/UserCenterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,41 +16,65 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: '首页',
+      },
     },
     {
       path: '/trade',
       name: 'trade',
-      component: () => import('@/views/TradeView.vue'),
+      component: TradeView,
+      meta: {
+        title: '二手交易',
+      },
     },
     {
       path: '/lost-found',
       name: 'lostFound',
-      component: () => import('@/views/LostFoundView.vue'),
+      component: LostFoundView,
+      meta: {
+        title: '失物招领',
+      },
     },
     {
       path: '/group-buy',
       name: 'groupBuy',
-      component: () => import('@/views/GroupBuyView.vue'),
+      component: GroupBuyView,
+      meta: {
+        title: '拼单搭子',
+      },
     },
     {
       path: '/errand',
       name: 'errand',
-      component: () => import('@/views/ErrandView.vue'),
+      component: ErrandView,
+      meta: {
+        title: '跑腿委托',
+      },
     },
     {
       path: '/publish',
       name: 'publish',
-      component: () => import('@/views/PublishView.vue'),
+      component: PublishView,
+      meta: {
+        title: '发布信息',
+      },
     },
     {
       path: '/message',
       name: 'message',
-      component: () => import('@/views/MessageView.vue'),
+      component: MessageView,
+      meta: {
+        title: '消息中心',
+      },
     },
     {
-      path: '/user-center',
-      name: 'userCenter',
-      component: () => import('@/views/UserCenterView.vue'),
+      path: '/user',
+      name: 'user',
+      component: UserCenterView,
+      meta: {
+        title: '个人中心',
+      },
     },
   ],
 })
