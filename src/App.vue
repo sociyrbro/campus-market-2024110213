922262c3aab808257e02e35aeca1c927ad585fdb
sociyrbro <1565321242@qq.com>
2026-06-28 09:38:32 +0800

    构建基础导航系统

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="app">
-    <h1>校园轻集市</h1>
-    <p>AI 辅助前端工程实践种子项目</p>
-    <RouterView />
-  </main>
-</template>
+  <nav>
+    <router-link to="/home">首页</router-link>
+    <router-link to="/list">列表</router-link>
+    <router-link to="/publish">发布</router-link>
+    <router-link to="/message">消息</router-link>
+    <router-link to="/profile">我的</router-link>
+  </nav>
 
+  <router-view />
+</template>
 <style scoped>
 .app {
   padding: 32px;
