3dc18c245afe75b2cd8416b6d9341dddb81b5cb6
sociyrbro <1565321242@qq.com>
2026-06-28 09:27:31 +0800

    完善路由系统

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,36 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/list',
+      name: 'list',
+      component: () => import('@/views/ListView.vue'),
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: () => import('@/views/DetailView.vue'),
+    },
+    {
+      path:'/publish',
+      name:'publish',
+      component:()=> import('@/views/PublishView.vue')
+    },
+    {
+      path:'/message',
+      name:'message',
+      component:()=> import('@/views/MessageView.vue')
+    },
+    {
+      path:'/profile',
+      name:'profile',
+      component:()=> import('@/views/ProfileView.vue')
+    },
+    {
+      path:'/board',
+      name:'board',
+      component:()=> import('@/views/BoardView.vue')
+    }
   ],
 })
 
